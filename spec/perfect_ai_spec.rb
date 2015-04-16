c578be07906856f991c6d20964ede5490246cc09
Brent Haas <brenthaas@gmail.com>
2015-04-15 20:37:30 -0700

    Updated to ruby 2.2.2 and changed to expect syntax

--- spec/perfect_ai_spec.rb
+++ spec/perfect_ai_spec.rb
@@ -10,7 +10,7 @@ describe PerfectAi do
 
   describe "#choose_location" do
     it "starts in the center square" do
-      subject.pick_move(game).should == 'B2'
+      expect(subject.pick_move(game)).to eq 'B2'
     end
   end
 
@@ -18,8 +18,7 @@ describe PerfectAi do
     it "takes an available win" do
       game.make_move player, 'A1'
       game.make_move player, 'A2'
-      subject.find_winning_row(game).should == %w(A1 A2 A3)
+      expect(subject.find_winning_row(game)).to == %w(A1 A2 A3)
     end
   end
-
 end
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,27 +1,34 @@
 require_relative '../tic_tac_toe.rb'
 
 describe TicTacToe do
-  let!(:game) {TicTacToe.new}
+  let(:game) { TicTacToe.new }
 
   describe "#valid_player?" do
+
     specify "player 'X' is valid" do
-      game.valid_player?('X').should be_true
+      expect(game.valid_player?('X')).to be true
     end
+
     specify "player 'O' is valid" do
-      game.valid_player?('O').should be_true
+      expect(game.valid_player?('O')).to be true
     end
+
     specify "player 'Z' is NOT valid" do
-      game.valid_player?('Z').should_not be_true
+      expect(game.valid_player?('Z')).to_not be true
     end
   end
 
   describe "#make_move" do
-    let(:player) { game.players.first }
+    let(:player) { TicTacToe::PLAYERS.first }
+
     specify "invalid location raises errors" do
       expect{ game.make_move(player, 'AB') }.to raise_error(InvalidMoveError)
     end
+
     it "fills in the square moved in" do
-      expect{game.make_move(player, 'A1') }.to change{game.board['A1'] }.from(' ').to(player)
+      expect{
+        game.make_move(player, 'A1')
+      }.to change{game.board['A1'] }.from(' ').to(player)
     end
   end
 
@@ -31,11 +38,12 @@ describe TicTacToe do
 
   describe "#new_game?" do
     it "is true for new games" do
-      game.new_game?.should be_true
+      expect(game.new_game?).to be true
     end
+
     it "is false after a move is made" do
       game.make_move('X', 'A1')
-      game.new_game?.should be_false
+      expect(game.new_game?).to be false
     end
   end
 end
